0f67ed603637c6ba599d5bef914b73c0295fda64
rrimm <qwerrimm@gmail.com>
2026-02-20 00:59:32 +0900

    fix logic for exercise 2 and update print messages

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,5 @@
-# Assignment 1
+# Chapter 4 - while loop
+# Exercise 1
 # Write a program that uses a while loop to print out all numbers divisible by three in the range of 1-1000.
 number = 0
 arr = []
@@ -10,33 +11,35 @@ while number < 1000:
 
 print(arr)
 
-# Assignment 2
+# Exercise 2
 # Write a program that converts inches to centimeters until the user inputs a negative value. Then the program ends.
-inches = float(input("Enter command: "))
-centimeters = inches * 0.393701
+inches = float(input("Enter value: "))
 
 while True:
-    if inches <= 0:
+    centimeters = inches * 2.54
+
+    if inches < 0:
         print("Inches cannot be a negative value")
         break
-    print(f'centimeters: {centimeters}')
-    inches = float(input("Enter command: "))
 
-# Assignment 3
+    print(f'centimeters: {centimeters}')
+    inches = float(input("Enter value: "))
+
+# Exercise 3
 # Write a program that asks the user to enter numbers until they enter an empty string to quit. Finally, the program prints out the smallest and largest number from the numbers it received.
 arr = []
 userNumber = input("Enter a number: ")
 
 while True:
     if userNumber == "":
-        print(f"max number: {max(arr)}")
-        print(f"min number: {min(arr)}")
+        print(f"the largest number: {max(arr)}")
+        print(f"the smallest number: {min(arr)}")
         break
 
     arr.append(int(userNumber))
     userNumber = input("Enter a number: ")
 
-# Assignment 4
+# Exercise 4
 # Write a game where the computer draws a random integer between 1 and 10. The user tries to guess the number until they guess the right number. After each guess the program prints out a text: Too high, Too low or Correct. Notice that the computer must not change the number between guesses.
 import random
 randomNumber = random.randint(1, 10)
@@ -55,7 +58,7 @@ while True:
 
     userNumber = int(input("Enter command: "))
 
-# Assignment 5
+# Exercise 5
 # Write a program that asks the user for a username and password. If either or both are incorrect, the program ask the user to enter the username and password again. This continues until the login information is correct or wrong credentials have been entered five times. If the information is correct, the program prints out Welcome. After five failed attempts the program prints out Access denied. The correct username is python and password rules.
 username = "rim"
 password = "0000"
